9ea245b520c3a2367b5a27d1a6b89750bf4e5608
Lukas Baumert <lukasbaumert1010@gmail.com>
2023-04-17 22:56:02 +0200

    met requirements

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const readFileAsync = promisify(fs.readFile);
-const args = process.argv.slice(2);
 
 // random index generator based on list length
 function getRandomIndex(list) {
